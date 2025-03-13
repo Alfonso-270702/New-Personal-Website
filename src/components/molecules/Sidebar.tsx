cc164cb95da952f55d00cc26d92d4c459be1eab0
Alfonso-270702 <tebaksiapa27@gmail.com>
2025-03-13 14:44:11 +0700

    change sidebar animation with motion

--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useNavigate, useLocation } from "react-router";
 import Backdrop from "../atoms/Backdrop";
 import { profileData } from "../../static/profileData";
 import ProfileCard from "../atoms/ProfileCard";
+import { motion, AnimatePresence } from "framer-motion"; // Perbaiki import
 
 interface SidebarProps {
   isSidebarOpen: boolean;
@@ -11,35 +12,43 @@ interface SidebarProps {
 const Sidebar = ({ isSidebarOpen, setSidebarOpen }: SidebarProps) => {
   const navigate = useNavigate();
   const location = useLocation();
+
   return (
     <>
       {/* Overlay untuk mencegah klik carousel saat sidebar terbuka */}
       <Backdrop isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Sidebar */}
-      <div
-        className={`fixed top-[100px] left-0 h-[calc(100vh-100px)] w-[593px] sidebar-open text-white shadow-lg border-2 border-t-0 transition-transform duration-300 ease-in-out z-50 ${
-          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-        onClick={() => setSidebarOpen(false)}
-      >
-        <ProfileCard
-          profileName={profileData.name}
-          description={profileData.desc}
-          isButton={true}
-          buttonFunction={() => {
-            navigate("/contact");
-            setSidebarOpen(false);
-          }}
-          buttonContent="GET CONNECTED"
-          containerClass="p-[32px] flex flex-col"
-          imgStyle="w-full h-[420px] object-cover"
-          profileNameStyle="text-[46px] font-bold"
-          profileDescStyle="text-[18px] text-justify"
-        />
-      </div>
+      <AnimatePresence>
+        {isSidebarOpen && (
+          <motion.div
+            key="sidebar"
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%", opacity: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="fixed top-[100px] left-0 h-[calc(100vh-100px)] w-[593px] sidebar-open text-white shadow-lg border-2 border-t-0 z-50"
+            onClick={() => setSidebarOpen(false)}
+          >
+            <ProfileCard
+              profileName={profileData.name}
+              description={profileData.desc}
+              isButton={true}
+              buttonFunction={() => {
+                navigate("/contact");
+                setSidebarOpen(false);
+              }}
+              buttonContent="GET CONNECTED"
+              containerClass="p-[32px] flex flex-col"
+              imgStyle="w-full h-[420px] object-cover"
+              profileNameStyle="text-[46px] font-bold"
+              profileDescStyle="text-[18px] text-justify"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-      {/* Tombol untuk membuka sidebar */}
+      {/* Tombol Sidebar yang muncul jika Sidebar tertutup */}
       {!isSidebarOpen && (
         <div
           onClick={() => {
